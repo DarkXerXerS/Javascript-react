ce3074151c4abe901d9754d0e5e52232aa690055
DarkXerXerS <darkxerxers29@gmail.com>
2024-08-14 14:24:19 -0700

    first commit on this project

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express()
+
+app.listen(2000, () => {
+    console.log("server connected successfully")
+})
+
+console.log("working with nodejs")
+
---
+++ code.js
@@ -0,0 +1,17 @@
+// LOGGING OUT SOMETHING TO THE CONSOLE
+console.log('Started learning javaScript again ');
+
+//CREATED A VARIABLE
+let myAge = 20;
+// CREATED A ANOTHER VARIABLE
+let earlyYears = 2; 
+earlyYears = earlyYears *= 10.5;
+// CRAETED A VARIABLE TO STORE THE RESULT OF myAge variable minius 2
+let laterYears = myAge -= 2
+laterYears *= 4;
+//ADDING TWO VARIABLE TOGETHER AND STORING THEM IN A VARIABLE
+let myAgeInDogYears = earlyYears + laterYears;
+//
+let myName = 'XERXERS'.toLowerCase();
+//
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
